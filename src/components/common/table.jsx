cb97633eb77638c53ee228999ba948999f4e4bf8
Anas Mannaa <30526159+anasmannaa@users.noreply.github.com>
2022-11-15 13:58:00 +0400

    Create table.jsx

---
+++ src/components/common/table.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import TableBody from "./tablebody";
+import TableHeader from "./tableheader";
+
+const Table = ({ columns, sortColumn, onSort, data }) => {
+  return (
+    <table className="table">
+      <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
+      <TableBody data={data} columns={columns} />
+    </table>
+  );
+};
+
+export default Table;
